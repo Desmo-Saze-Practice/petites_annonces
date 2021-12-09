f0706f7035cb68be2dda504c49392656ca3c6941
adguernier <adguernier@gmail.com>
2021-12-09 14:36:14 +0100

    première route avec annotation

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,10 +4,13 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-
+use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
-    public function index()
+    /**
+     * @Route("/")
+     */
+    public function index(): Response
     {
         // affiche le fichier index.html.twig
         return $this->render('home/index.html.twig');
