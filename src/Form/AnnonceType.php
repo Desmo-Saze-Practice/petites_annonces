026d37c5c24b0bb7832f710fa90163b94198ad7d
adguernier <adguernier@gmail.com>
2022-01-18 14:23:11 +0100

    champ select pour le status d'une annonce

--- src/Form/AnnonceType.php
+++ src/Form/AnnonceType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Annonce;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -19,7 +20,15 @@ class AnnonceType extends AbstractType
             ->add('price', MoneyType::class, [
                 'divisor' => 100
             ])
-            ->add('status')
+            ->add('status', ChoiceType::class, [
+                'choices' => [
+                    'annonce.status_0' => Annonce::STATUS_VERY_BAD,
+                    'annonce.status_1' => Annonce::STATUS_BAD,
+                    'annonce.status_2' => Annonce::STATUS_GOOD,
+                    'annonce.status_3' => Annonce::STATUS_VERY_GOOD,
+                    'annonce.status_4' => Annonce::STATUS_PERFECT
+                ]
+            ])
         ;
     }
 
@@ -27,6 +36,7 @@ class AnnonceType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => Annonce::class,
+            'translation_domain' => 'annonce'
         ]);
     }
 }
