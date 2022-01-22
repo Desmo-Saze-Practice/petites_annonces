d15be4ea03b4db12cca328f19e8b49550d8adf09
florian <martinflorian2@gmail.com>
2022-01-22 18:46:48 +0100

    suposively dynamix routes annonce

--- src/Controller/AnnonceController.php
+++ src/Controller/AnnonceController.php
@@ -9,12 +9,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class AnnonceController extends AbstractController
 {
     /**
-     * @Route("/annonce", name="annonce")
+     * @Route("/annonces", name="annonce")
      */
     public function index(): Response
     {
         return $this->render('annonce/index.html.twig', [
             'controller_name' => 'AnnonceController',
+            'current_annonce' => 'app_annonce_index',
         ]);
     }
 }
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,6 +6,7 @@ use DateTimeZone;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+// use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 class HomeController extends AbstractController
 {
@@ -19,6 +20,7 @@ class HomeController extends AbstractController
             'title' => $title,
             'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem quam cum corrupti modi cupiditate nostrum odit illo veniam, nulla neque officia expedita rerum, aliquid libero incidunt rem iusto reprehenderit maxime!',
             'date' => new \DateTime(),
+            'current_annonce' => "app_annonce_index",
         ]);
     }
 }
