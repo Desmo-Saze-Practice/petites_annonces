5cbc3cd231eb36c294e40f792f4646b02db2810d
florian <martinflorian2@gmail.com>
2022-01-23 12:10:56 +0100

    create annonce function and display them

--- src/Controller/AnnonceController.php
+++ src/Controller/AnnonceController.php
@@ -2,20 +2,59 @@
 
 namespace App\Controller;
 
+use App\Entity\Annonce;
+use App\Repository\AnnonceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bridge\Doctrine\ManagerRegistry;
 
 class AnnonceController extends AbstractController
 {
     /**
      * @Route("/annonces", name="annonce")
      */
-    public function index(): Response
-    {
+    public function index(AnnonceRepository $annonceRepository): Response
+    {       
+        //recherche par ID
+        $annonce = $annonceRepository->find(1);
+
+        // recherche toutes les annonces
+        $annonce = $annonceRepository->findAll();
+
+        // rechercher une annonce par champ
+        $annonce = $annonceRepository->findOneBy(['sold' => false]);
+
+        // recherche par non vendu
+        $annonces = $annonceRepository->findAllNotSold();
+
+        dump($annonce);
+        dump($annonces);
+
         return $this->render('annonce/index.html.twig', [
-            'controller_name' => 'AnnonceController',
-            'current_annonce' => 'app_annonce_index',
+            'annonces' => $annonces,
         ]);
+    }
+
+    /**
+     * @Route("/annonces/new")
+     */
+    public function new()
+    {
+        $annonce = new Annonce();
+        $annonce
+            ->setTitle('Sabre lazer peu utilisé')
+            ->setDescription('Couleur bleu, très coupant. Il peut aussi vous tenir chaud en hiver.')
+            ->setPrice(8000)
+            ->setStatus(Annonce::STATUS_BAD)
+            ->setSold(false);
+
+            // on utilise EntityManager pour inserer notre nouvelle entité
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($annonce);
+            // et on envoie
+            $em->flush();
+
+            die('Annonce créée');
     }
 }
---
+++ src/Entity/Annonce.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AnnonceRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=AnnonceRepository::class)
+ */
+class Annonce
+{
+    const STATUS_VERY_BAD = 0;
+    const STATUS_BAD = 1;
+    const STATUS_GOOD = 2;
+    const STATUS_VERY_GOOD = 3;
+    const STATUS_VERY_PERFECT = 4;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTime();
+    }
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="boolean", options={"default": false})
+     */
+    private $sold = false;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $status;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getSold(): ?bool
+    {
+        return $this->sold;
+    }
+
+    public function setSold(bool $sold): self
+    {
+        $this->sold = $sold;
+
+        return $this;
+    }
+
+    public function getStatus(): ?int
+    {
+        return $this->status;
+    }
+
+    public function setStatus(int $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
